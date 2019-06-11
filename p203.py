8037c2c4324377c967fa07dcb6fe0e4702fb233c
dhivya2nandha <50711793+dhivya2nandha@users.noreply.github.com>
2019-06-11 11:42:30 +0530

    Create p203.py

---
+++ p203.py
@@ -0,0 +1,14 @@
+def count(s):
+    k1=0
+    for i in s:
+        if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+            k1=k1+1
+    return k1
+n=int(input())
+l=[]
+for i in range(n):
+    s=input()
+    l.append([s,count(s)])
+l.sort(key=lambda x:x[1],reverse=True)
+for i in range(0,len(l)):
+    print(l[i][0])
